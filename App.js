8910a1596a819667ccbfceb390ba23e67bbef63b
Nithin Modiboyana <nithinmodiboyana@gmail.com>
2024-04-02 13:41:52 +0530

    episode-3 Laying the foundation

--- App.js
+++ App.js
@@ -1,50 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-/* 
+//react element
+const heading  = React.createElement(
+  "h1",
+  {id:"heading"},
+  "Namste react using React.createElement 🚀"
+)
 
-<div id="parent">
-  <div id=child>
-    <h1>I'm h1 Tag</h1>
-    <h2>I'm h2 Tag</h2>
+console.log(heading);
+//jsx - html like syntax babel transpiles this into react element 
+const jsxHeading = (
+    <h1 id="heading" className='head'>
+      {heading}
+
+      Namste react using jsx 🚀
+    </h1>
+);
+/*
+react component
+there are 2 types of components:
+class based component - old
+functional component - new
+*/ 
+// react functional component is a function that returns a jsx code
+const TitleComponent =()=>(
+  <div className='container'>
+    <h1 className='headingComponent'>Namste react Title component</h1>
   </div>
-  <div id=child2>
-    <h1>I'm h1 Tag</h1>
-    <h2>I'm h2 Tag</h2>
+);
+// component composition puting one component in another component
+const HeadingComponent =()=>(
+  <div className='container'>
+    {heading/*  {inside brackets you can write react element or any js code}*/ }
+    <TitleComponent />
+    {TitleComponent()}
+    <TitleComponent></TitleComponent>
+    <h1 className='headingComponent'>Namste react functional component</h1>
   </div>
-</div>
+);
 
-ReactElement(Object) => HTML(Browser Understands)
+console.log(jsxHeading);
 
-*/
-
-const parent = React.createElement('div', { id: 'parent' }, [
-  React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, "I'm h1 Tag"),
-    React.createElement('h2', {}, "I'm h2 Tag"),
-  ]),
-  React.createElement('div', { id: 'child2' }, [
-    React.createElement('h1', {}, "I'm h1 Tag"),
-    React.createElement('h2', {}, "I'm h2 Tag"),
-  ]),
-]);
-
-// JSX
-
-console.log(parent);
-
-// const heading = React.createElement(
-//   'h1',
-//   { id: 'heading', className: 'head' },
-//   'Hello World from React!'
-// ); // It's the job of Core React to create an element i.e, heading in this case
-
-// React Element is normal JavaScript Object{}
-
-// console.log(heading); // object
-
-const root = ReactDOM.createRoot(document.getElementById('root')); // It's the job of ReactDOM to create root i.e, heading in this case, bcz root is a dom element which is rendered to be on browser. This root is the place where all React code will run.
-
-// root.render(heading);
-root.render(parent);
-
-// In React, Render is the technique that can redirect a page with the help of function render(). Most importantly, render a function we can use to define the HTML code within the HTML element. It helps to display certain views in the UI using certain logic defined in the render function and returns the output.
+const root =  ReactDOM.createRoot(document.getElementById("root"));
+//react element rendering
+//root.render(jsxHeading);
+// react component rendering
+root.render(<HeadingComponent />)
